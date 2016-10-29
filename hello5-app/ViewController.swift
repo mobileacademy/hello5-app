67c49364d0045582440f01abe15683d5e04d017b
Mihai Iancu <miancu@adobe.com>
2016-10-29 10:32:44 +0300

    Initial Commit

---
+++ hello5-app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hello5-app
+//
+//  Created by Mihai Iancu on 29/10/2016.
+//  Copyright © 2016 MA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
