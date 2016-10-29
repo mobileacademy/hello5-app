de009b696d6aec511350612066d5e61657f9bb0b
Mihai Iancu <miancu@adobe.com>
2016-10-29 11:37:55 +0300

    step2

--- hello5-app/AppDelegate.swift
+++ hello5-app/AppDelegate.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 
+import SCFacebook
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -17,6 +19,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
+    }
+    
+    func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
+        return FBSDKApplicationDelegate.sharedInstance().application(application, open: url, sourceApplication: sourceApplication, annotation: annotation)
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
--- hello5-app/ViewController.swift
+++ hello5-app/ViewController.swift
@@ -7,12 +7,42 @@
 //
 
 import UIKit
+import SCFacebook
 
 class ViewController: UIViewController {
+    
+    var name:String?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        SCFacebook.initWithReadPermissions(["email","public_profile"], publishPermissions: [])
+        
+        if FBSDKAccessToken.current() != nil {
+            self.getFacebookUser()
+        }else{
+        
+            SCFacebook.loginCallBack { (success, result) in
+            SCFacebook.getUserFields("email", callBack: { (success, result) in
+                if !success {
+                    print("failed to login")
+                    return
+                }
+                self.getFacebookUser()
+            })
+            }
+        }
+    }
+    
+    func getFacebookUser (){
+        FBSDKGraphRequest.init(graphPath: "me", parameters: nil).start(completionHandler: { (connection, result, error) in
+            let dict = result as! [String : AnyObject]
+            let user_id = dict["id"] as! String
+            let name = dict["name"] as! String
+            
+            print(name)
+            self.name = name
+        })
     }
 
     override func didReceiveMemoryWarning() {
